99c340df049ba4debace9beb3f4f1dbbcb75bed6
Antonio Vieira <vieira.n55@gmail.com>
2024-02-22 15:36:26 -0300

    feat: mobile finalizado

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 function cript(){
-  var textoInput = document.getElementById("input").value.toLowerCase();
-  var novaString = textoInput.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
-
-  var cripto = document.getElementById("criptografado");
-  cripto.innerHTML = novaString;
-
+  if (document.getElementById("input").value.toLowerCase() !== ""){
+    var textoInput = document.getElementById("input").value.toLowerCase();
+    var novaString = textoInput.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+  
+    document.getElementById("criptografado").style.display = "none";
+    document.getElementById("criptografado_sucess").style.display = "flex";
+  
+    var cripto = document.getElementById("resultado");
+    cripto.innerHTML = novaString;
+  }
 }
 
 function descript(){
-  var textoInput = document.getElementById("input2").value.toLowerCase();
-  var novaString = textoInput.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u")
+  if (document.getElementById("input").value.toLowerCase() !== ""){
+    var textoInput = document.getElementById("input").value.toLowerCase();
+    var novaString = textoInput.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+  
+    document.getElementById("criptografado").style.display = "none";
+  
+  
+    document.getElementById("criptografado_sucess").style.display = "flex";
+  
+    var cripto = document.getElementById("resultado");
+    cripto.innerHTML = novaString;
+  }
+}
 
-  var cripto = document.getElementById("decriptografado");
-  cripto.innerHTML = novaString;
-
+async function copiar(){
+  let text = document.getElementById('resultado').innerHTML;
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
 }
